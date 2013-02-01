138b0bff1df499987206854fb6f59a69cc16d732
Romain Tartière <romain@blogreen.org>
2013-02-01 11:46:54 +0100

    Refactor code, add methods and documentation.

--- lib/twitter_bootstrap_combo/view_helpers.rb
+++ lib/twitter_bootstrap_combo/view_helpers.rb
@@ -1,13 +1,69 @@
 module TwitterBootstrapCombo
   module ViewHelpers
+    # Accepts a container and return a string of combo_box items. If +selected+
+    # is specified, the corresponding item is marked as active.
+    def options_for_combo_box(container, selected = nil)
+      return container if String === container
+
+      container.map do |element|
+        if Array === element then
+          text, value = element
+        else
+          text, value = element, element
+        end
+        css_class = []
+        if selected == value then
+          css_class << "active"
+        end
+        content_tag(:li, :class => css_class.join(" ")) do
+          link_to(text, '#', :class => "combo_box_item", :data => { :value => value })
+        end
+      end.join().html_safe
+    end
+
+    # Returns a string of combo_box items that have been compiled by iterating
+    # over the +collection+ and assigning the result of the call to the
+    # +value_method+ as the item value and the +text_method+ as the item text.
+    # If +selected+ is specified, the item with the corresponding value is
+    # marked as selected.
+    def options_from_collection_for_combo_box(collection, value_method, text_method, selected = nil)
+      options = collection.map do |element|
+        [ element.send(text_method), element.send(value_method) ]
+      end
+
+      options_for_combo_box(options, selected)
+    end
+
+    # Returns a complete Twitter Bootstrap widget tailored for accessing a
+    # specified attribute (identified by +method+) on an object assigned to the
+    # template (identified by +object+) using a Twitter Bootstrap menu with
+    # +choices+. Additional options can be passed as a hash with +options+ and
+    # +html_options+.
+    #
+    # Supported +options+:
+    # - <tt>:include_blank</tt> - If set to true, an empty option will be created.
+    #
+    # Supported +html_options+:
+    # - None yet
+    #
+    # Examples:
+    #   combo_box(:event, :calendar_id, [ [ "Calendar 1", 1 ], [ "Calendar 2", 2] ])
+    #
+    #   container(:event, :calendar_id, options_from_collection_for_combo_box(Calendar.all, :id, :name, @event.calendar_id), :include_blank => true)
     def combo_box(object, method, choices, options = {}, html_options = {})
       current_value = self.instance_variable_get("@#{object}").instance_variable_get("@attributes")[method.to_s].to_i
 
-      curent = choices.select { |c| c[1] == current_value }.first
-      if curent.nil? then
-        current_text = "(None)"
-      else
-        current_text = curent[0]
+      current_text = "(None)"
+      if Array === choices then
+        if ! Array === choices[0] then
+          current_text = current_value
+        else
+          choices.each do |element|
+            if current_value == element[1] then
+              current_text = element[0]
+            end
+          end
+        end
       end
 
       hidden_field(object, method) +
@@ -25,15 +81,7 @@ module TwitterBootstrapCombo
           else
             "".html_safe
           end +
-          choices.collect do |o|
-            css_class = []
-            if current_value == o[1] then
-              css_class << "active"
-            end
-            content_tag(:li, :class => css_class.join(" ")) do
-              link_to(o[0], '#', :class => "combo_box_item", :data => { :value => o[1] })
-            end
-          end.join().html_safe
+          options_for_combo_box(choices, current_value)
         end
       end
     end
--- vendor/assets/javascripts/twitter_bootstrap_combo.js.coffee
+++ vendor/assets/javascripts/twitter_bootstrap_combo.js.coffee
@@ -1,4 +1,7 @@
 jQuery ->
+  $(".combo_box_text").each( ->
+    $(this).text($(this).closest("div.btn-group").find("ul").find("li.active").text())
+  )
   $('a.combo_box_item').click( ->
     $("#" + $(this).parent("li").parent("ul").attr("data-for")).val($(this).attr("data-value"))
     $(this).closest(".btn-group").find(".combo_box_text").text($(this).text())
